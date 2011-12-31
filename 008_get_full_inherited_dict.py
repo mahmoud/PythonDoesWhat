4406012e33af253ddd218888d3e4140c5d0a826f
Mahmoud Hashemi <makuro@gmail.com>
2011-12-31 01:23:44 -0800

    slowly progressing, fixed a naming bug

---
+++ 008_get_full_inherited_dict.py
@@ -0,0 +1,32 @@
+#/bin/env python
+"""
+NOTE: You probably want to use the inspect module's getmembers() function.
+
+>>> b = B()
+>>> b.testb = "b"
+>>> b_dict = get_full_dict(b)
+>>> b_dict["testA"]
+'A'
+>>> b_dict["testB"]
+'B'
+>>> b_dict["testb"]
+'b'
+"""
+
+class A(object):
+    testA = "A"
+    testB = "A"
+    testb = "A"
+
+class B(A):
+    testB = "B"
+    testb = "B"
+
+def get_full_dict(obj):
+    parent_dicts = [obj.__dict__] + [cls.__dict__ for cls in obj.__class__.__mro__ if cls.__name__ != "object"]
+    return reduce(lambda a,b: dict(b.items() + a.items()), parent_dicts, {})
+
+
+title = "Get full inherited __dict__"
+author = "Kurt"
+date = (2010, 12, 6, 10, 57)
---
+++ 009_attributedict.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+"""
+attributedict is a dictionary whose keys are also accessible as object attributes. No doubt JavaScripters out there will find it pretty familiar.
+
+MH: This incredibly simple recipe is probably one of my favorites. For most intents and purposes, it supplants the whole `**Bunch** Python package<http://pypi.python.org/pypi/bunch>`_ in four lines.
+
+>>> ad = attributedict()
+>>> ad["one"] = 1
+>>> ad.one
+1
+>>> ad.two = 2
+>>> ad["two"]
+2
+>>> attributedict(three=3).three
+3
+
+Also, we found `an incredibly similar recipe on ActiveState code<http://code.activestate.com/recipes/361668/>`_. From 2005!
+"""
+
+class attributedict(dict):
+    def __init__(self, *a, **kw):
+        self.__dict__ = self
+        dict.__init__(self, *a, **kw)
+
+title = "Attributedict: dictionary whose keys are also attributes"
+author = Kurt
+date = (2010, 12, 7, 16, 29)
